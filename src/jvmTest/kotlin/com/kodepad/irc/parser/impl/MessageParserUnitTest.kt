6b34cf27a96780b40cb1979ee90b1600d6a0ad53
Dipendra Singh <dipendra32singh@gmail.com>
2021-01-20 08:56:13 +0530

    Add a failing parsing test

--- src/jvmTest/kotlin/com/kodepad/irc/parser/impl/MessageParserUnitTest.kt
+++ src/jvmTest/kotlin/com/kodepad/irc/parser/impl/MessageParserUnitTest.kt
@@ -123,4 +123,14 @@ class MessageParserUnitTest {
         val expectedMatchedString = ":dummykodepadnick MODE dummykodepadnick :+i\r\n"
         assertEquals(expectedMatchedString, ast.matchedString)
     }
+
+    @Test
+    fun `parsing example 12`() {
+        val input = ":freenode-connect!frigg@freenode/utility-bot/frigg NOTICE dummykodepadnick :Welcome to freenode. To protect the network all new connections will be scanned for vulnerabilities. This will not harm your computer, and vulnerable hosts will be notified.\r\n"
+
+        val ast = parser.parse(input)
+
+        val expectedMatchedString = ":dummykodepadnick MODE dummykodepadnick :+i\r\n"
+        assertEquals(expectedMatchedString, ast.matchedString)
+    }
 }

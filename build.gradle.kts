67826f25347567f8e9b50edf3ebac8014681bc5f
Dipendra Singh <dipendra32singh@gmail.com>
2021-03-13 22:20:03 +0530

    Disable js artifacts, change project name

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,40 @@
 plugins {
     kotlin("multiplatform") version "1.4.21"
+    id("maven-publish")
 }
+
 group = "com.kodepad.irc"
 version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
+
 kotlin {
     jvm {
         compilations.all {
             kotlinOptions.jvmTarget = "1.8"
         }
     }
-    js {
-        browser {
-            testTask {
-                useKarma {
-                    useChromeHeadless()
-                    webpackConfig.cssSupport.enabled = true
-                }
-            }
-        }
-    }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+//    js {
+//        browser {
+//            testTask {
+//                useKarma {
+//                    useChromeHeadless()
+//                    webpackConfig.cssSupport.enabled = true
+//                }
+//            }
+//        }
+//    }
+//
+//    val hostOs = System.getProperty("os.name")
+//    val isMingwX64 = hostOs.startsWith("Windows")
+//    val nativeTarget = when {
+//        hostOs == "Mac OS X" -> macosX64("native")
+//        hostOs == "Linux" -> linuxX64("native")
+//        isMingwX64 -> mingwX64("native")
+//        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+//    }
 
     
     sourceSets {
@@ -48,19 +52,22 @@ kotlin {
                 implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.0")
             }
         }
+
         val jvmMain by getting
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
             }
         }
-        val jsMain by getting
-        val jsTest by getting {
-            dependencies {
-                implementation(kotlin("test-js"))
-            }
-        }
-        val nativeMain by getting
-        val nativeTest by getting
+
+//        val jsMain by getting
+//        val jsTest by getting {
+//            dependencies {
+//                implementation(kotlin("test-js"))
+//            }
+//        }
+//
+//        val nativeMain by getting
+//        val nativeTest by getting
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,1 @@
-
-rootProject.name = "IrcClient"
-
+rootProject.name = "irc-client-core"
